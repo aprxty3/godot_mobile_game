29c0d0eba8438f200e00e5a83d69dc1160d3e989
aji prasetyo <ajiprxty3@gmail.com>
2024-07-10 19:49:51 +0700

    create bird

---
+++ scripts/bird.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+signal gameover
+
+@onready var animate_sprite = $AnimatedSprite2D
+
+const JUMP_VELOCITY = -300.0
+const ROT_SPEED = 300.0
+var velocity : Vector2 = Vector2.ZERO
+
+func _ready():
+	gravity = 500.0
+	random_bird()
+	
+
+func random_bird():
+	var animation = animate_sprite.sprite_frames.get_animation_names()
+	print(animation)
+	animate_sprite.animation = animation[randi() % animation.size()]
+
+func _process(delta):
+	velocity.y += gravity * delta
+	
+	if rotation_degrees <= 45 and velocity.y > 0:
+		rotation_degrees += ROT_SPEED * delta
+	elif rotation_degrees >= -45:
+			rotation_degrees -= ROT_SPEED * delta
+		
+	if Input.is_action_just_pressed("tap"):
+		velocity.y = JUMP_VELOCITY
+		animate_sprite.play()
+	
+	position += velocity*delta
+
+
+func _on_body_entered(body):
+	animate_sprite.stop()
+	gameover.emit()
